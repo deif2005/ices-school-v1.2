a80d5ec782dd735d45fc6c0407227cd6305fc7fa
defi2015 <11107040@qq.com>
2017-03-06 20:34:16 +0800

    test2

--- common/src/main/java/cn/mteach/common/Constants.java
+++ common/src/main/java/cn/mteach/common/Constants.java
@@ -3,6 +3,7 @@ package cn.mteach.common;
 public class Constants {
 	//defi 2017-03-06_git
 	//test1
+
 	public final static String ANSWERSHEET_DATA_QUEUE = "examplus.answersheetToScoreMaker";
 	public final static String CONFIG_PATH = "/opt/scoremarker";
 
